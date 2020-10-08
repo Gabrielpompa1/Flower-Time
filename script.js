864fe51329de1c8a136e5d46bfdc7645239e4e8c
Gabrielpompa1 <gabrielpompa@email.arizona.edu>
2020-10-08 11:39:26 -0700

    Main game screen layout / CSS established

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Ready to work')
+// Goals
+//Make flower
+// Choose a word to  make
+// Put it into and array, split it up by individual letters?
+// Make a form to 'submit' for the correct word choice
+// make a separate 'input' box for letter choices. If a letter input matches a value for the word, allow it to be shown. If now, put it in the bad guess category
+
+const previousGuesses = document.querySelector('.previouss-guesses')
+const correctGuesses = document.querySelector('.correct-guesses')
