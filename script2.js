8114ca7ce87691282400e9a9082cee0f12eacff3
Gabrielpompa1 <gabrielpompa@email.arizona.edu>
2020-10-10 20:48:24 -0700

    Need to continue working on polishing the submit guess function and uploading incorrect guesses to the right box. Made the rules button an alert for now to make it feel more solid.

--- script2.js
+++ script2.js
@@ -1,76 +1,97 @@
 //// Variables
 
+const word = ['F', 'I', 'N', 'D', 'S'];
+const correctUl = document.querySelector('.correct-ul');
+const incorrectUl = document.querySelector('.incorrect-ul');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const startButton = document.querySelector('.start-button');
+const rulesButton = document.querySelector('.open-rules')
 
-const word2 = ['F', 'I', 'N', 'D', 'S']
-const word = ['H', 'E', 'L', 'L', 'O']
-const correctUl = document.querySelector('.correct-ul')
-const incorrectUl = document.querySelector('.incorrect-ul')
-const form = document.querySelector('.form')
-const input = document.querySelector('.input')
-const startButton = document.querySelector('.start-button')
-let answer = []
-counter = 0
+
+let answer = [];
+counter = 0;
 ///// Event Listeners /////
-
 
 // Form event listener
 
 form.addEventListener('submit', (event) => {
-	event.preventDefault()
-	submitGuess()
-	checkWin()
-})
+	event.preventDefault();
+	submitGuess();
+	checkWin();
+});
 
 // Start Game event listener
 
 startButton.addEventListener('click', (event) => {
-	word2.forEach(makeUnderScore)
+	word.forEach(makeUnderScore);
+});
+
+// Show Rules event listener
+
+rulesButton.addEventListener('click', (event) => {
+	alert(`Welcome to Flower Time!\n \n In order to keep the flower tall and happy, you must guess its favorite word! \n\n Here are the rules: \n - In the 'Correct' box, input your guess for which letters might be found in the magic word. \n - If your guess is correct, the flower will remain intact. \n - If you guess wrong, the flower will lose a petal. \n - If the flower loses all its petals, the game is over. \n - If all of your guesses are correct and you spell the magic word, you win! \n - When you are ready, press start game. Good Luck!\n`)
 })
 
-
-
 ///// Functions /////
-
 
 // Check win condition function
 
 function checkWin() {
-	if(answer.toString() === word2.toString()) {
-		console.log('You Win')
+	if (answer.length === word.length) {
+		alert('You Win');
 	}
 }
 
 // Submit guess function
 
 function submitGuess() {
-	for (let i = 0; i < word2.length; i++) {
-		if(input.value.toUpperCase() === word2[i]) {
-			answer.push(input.value.toUpperCase())
-			const p = document.createElement('p')
-			p.classList.add('answer')
-			p.innerText = word2[i]
-			correctUl.appendChild(p)
-			input.value = ''
-		} 
-	}
+		if (input.value.toUpperCase() === word[0]) {
+			answer.push(input.value.toUpperCase());
+			let key0 = document.getElementById('key0')
+			key0.innerText = word[0]
+			input.value = '';
+		} else if (input.value.toUpperCase() === word[1]) {
+			answer.push(input.value.toUpperCase());
+			let key1 = document.getElementById('key1');
+			key1.innerText = word[1];
+			input.value = '';
+		} else if (input.value.toUpperCase() === word[2]) {
+			answer.push(input.value.toUpperCase());
+			let key2 =document.getElementById('key2');
+			key2.innerText = word[2];
+			input.value = '';
+		} else if (input.value.toUpperCase() === word[3]) {
+			answer.push(input.value.toUpperCase());
+			let key3 = document.getElementById('key3');
+			key3.innerText = word[3];
+			input.value = '';
+		} else if (input.value.toUpperCase() === word[4]) {
+			answer.push(input.value.toUpperCase());
+			let key4 = document.getElementById('key4');
+			key4.innerText = word[4];
+			input.value = '';
+		}
 }
-
 // Incorrect guess function
 
 function incorrectGuess() {
-	
+	if(input.value !== word) {
+
+	}
 }
 
 // Generate underscore spots for letter choices
 
 function makeUnderScore() {
-	let underscore = document.createElement('p')
-	underscore.classList.add('answer')
-	underscore.setAttribute('id', 'key' + counter++)
-	underscore.innerText = ('_')
-	correctUl.appendChild(underscore)
+	let underscore = document.createElement('p');
+	underscore.classList.add('answer');
+	underscore.setAttribute('id', 'key' + counter++);
+	underscore.innerText = '_';
+	correctUl.appendChild(underscore);
 }
 
+// Show Rules
 
 
 
@@ -81,7 +102,6 @@ function makeUnderScore() {
 // const correctUl = document.querySelector('.correct-ul')
 // const incorrectUl = document.querySelector('.incorrect-ul')
 // const winCountDiv = document.querySelector('.win-count')
-
 
 // let key0 = document.getElementById('key0')
 // let key1 = document.getElementById('key1')
@@ -142,12 +162,12 @@ function makeUnderScore() {
 // 	let newArr = key0.innerText + key1.innerText + key2.innerText + key3.innerText + key4.innerText
 // 	if(newArr === word){
 // 	alert('You win!')
-	
+
 // }
 // }
 
 // function winCount() {
-	
+
 // }
 
 // // function submitGuess() {
