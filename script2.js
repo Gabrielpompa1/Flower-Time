f5533dcb82feed317096144c319df54d5be60373
Gabrielpompa1 <gabrielpompa@email.arizona.edu>
2020-10-12 16:47:32 -0700

    Set themes for different costumes.

--- script2.js
+++ script2.js
@@ -1,36 +1,46 @@
 //// Variables
-
+const html = document.querySelector('HTML');
 const word = ['H', 'O', 'W', 'D', 'Y'];
-const word2 = ['F', 'L', 'O', 'W', 'E', 'R']
+const word2 = ['F', 'L', 'O', 'W', 'E', 'R'];
 const correctUl = document.querySelector('.correct-ul');
 const incorrectUl = document.querySelector('.incorrect-ul');
 const form = document.querySelector('.form');
 const input = document.querySelector('.input');
 const startButton = document.querySelector('.start-button');
-const rulesButton = document.querySelector('.open-rules')
-const resetButton = document.querySelector('.reset-button')
-const guessReaction = document.querySelector('.guess-reaction')
+const rulesButton = document.querySelector('.open-rules');
+const resetButton = document.querySelector('.reset-button');
+const guessReaction = document.querySelector('.guess-reaction');
 
+const costumeSubmit = document.querySelector('.costume-submit');
+const selectCostume = document.querySelector('.accessorize');
+const option = document.getElementsByTagName('option');
+const cowboyOption = document.querySelector('.cowboy-option');
+const spaceOption = document.querySelector('.space-option');
 
-const accessoryButton = document.querySelector('.accessorize')
-const cowboy = document.querySelectorAll('.cowboy')
-const baseLeft = document.querySelector('#base-left')
-const baseRight = document.querySelector('#base-right')
-const moustacheLeft = document.querySelector('#moustache-left')
-const moustacheRight = document.querySelector('#moustache-right')
-const brim = document.querySelector('#brim')
+const cowboy = document.querySelectorAll('.cowboy');
+const baseLeft = document.querySelector('#base-left');
+const baseRight = document.querySelector('#base-right');
+const moustacheLeft = document.querySelector('#moustache-left');
+const moustacheRight = document.querySelector('#moustache-right');
+const brim = document.querySelector('#brim');
 
-const smile = document.querySelector('#smile')
-const petal1 = document.querySelector('#petal1')
-const petal2 = document.querySelector('#petal2')
-const petal3 = document.querySelector('#petal3')
-const petal4 = document.querySelector('#petal4')
-const petal5 = document.querySelector('#petal5')
-const petal6 = document.querySelector('#petal6')
-const petal7 = document.querySelector('#petal7')
-const petal8 = document.querySelector('#petal8')
+const space = document.querySelector('.space-suit');
+const helmetTop = document.querySelector('#helmet-top');
+const helmetBase = document.querySelector('#helmet-base');
+const leftArm = document.querySelector('#left-arm');
+const rightArm = document.querySelector('#right-arm');
 
-let wrongAnswers = []
+const smile = document.querySelector('#smile');
+const petal1 = document.querySelector('#petal1');
+const petal2 = document.querySelector('#petal2');
+const petal3 = document.querySelector('#petal3');
+const petal4 = document.querySelector('#petal4');
+const petal5 = document.querySelector('#petal5');
+const petal6 = document.querySelector('#petal6');
+const petal7 = document.querySelector('#petal7');
+const petal8 = document.querySelector('#petal8');
+
+let wrongAnswers = [];
 let answer = [];
 counter = 0;
 ///// Event Listeners /////
@@ -39,8 +49,6 @@ counter = 0;
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	checkCorrectGuess()
-	checkIncorrectGuess()
 	submitGuess();
 	checkWin();
 });
@@ -49,30 +57,34 @@ form.addEventListener('submit', (event) => {
 
 startButton.addEventListener('click', (event) => {
 	word.forEach(makeUnderScore);
-	input.toggleAttribute('disabled')
-	startButton.toggleAttribute('disabled')
-	startButton.setAttribute('class', 'closed')
-	input.setAttribute('class', 'input-open')
+	input.toggleAttribute('disabled');
+	startButton.toggleAttribute('disabled');
+	startButton.setAttribute('class', 'closed');
+	input.setAttribute('class', 'input-open');
 });
 
 // Show Rules event listener
 
 rulesButton.addEventListener('click', (event) => {
-	showRules()
-})
+	showRules();
+});
 
 // Reset game event listener
 
 resetButton.addEventListener('click', (event) => {
-	location.reload()
-})
+	location.reload();
+});
 
-// Accessory switch event listener
+// Costume switch event listener
 
-accessoryButton.addEventListener('click', (event) => {
-	event.preventDefault()
-	showCowboy()
-})
+costumeSubmit.addEventListener('click', (event) => {
+	if (selectCostume.value === 'cowboy') {
+		showCowboy();
+	} else if (selectCostume.value === 'spaceman') {
+		showSpaceSuit();
+	}
+	// console.log(selectCostume.value)
+});
 
 ///// Functions /////
 
@@ -80,11 +92,11 @@ accessoryButton.addEventListener('click', (event) => {
 
 function checkWin() {
 	if (answer.length === word.length) {
-		smile.setAttribute('id', 'happy')
-		guessReaction.innerText = 'You win!'
+		smile.setAttribute('id', 'happy');
+		guessReaction.innerText = 'You win!';
 		input.toggleAttribute('disabled');
-	} else if(wrongAnswers.length === 8) {
-		guessReaction.innerText = 'You lose. Try again!'
+	} else if (wrongAnswers.length === 8) {
+		guessReaction.innerText = 'You lose. Try again!';
 		input.toggleAttribute('disabled');
 	}
 }
@@ -92,44 +104,46 @@ function checkWin() {
 // Word 1 submit guess function
 
 function submitGuess() {
-		if (input.value.toUpperCase() === word[0]) {
-			answer.push(input.value.toUpperCase());
-			guessCorrect()
-			smile.setAttribute('id', 'smile');
-			let key0 = document.getElementById('key0')
-			key0.innerText = word[0]
-			input.value = '';
-		} else if (input.value.toUpperCase() === word[1]) {
-			answer.push(input.value.toUpperCase());
-			guessCorrect()
-			smile.setAttribute('id', 'smile');
-			let key1 = document.getElementById('key1');
-			key1.innerText = word[1];
-			input.value = '';
-		} else if (input.value.toUpperCase() === word[2]) {
-			answer.push(input.value.toUpperCase());
-			guessCorrect()
-			smile.setAttribute('id', 'smile');
-			let key2 =document.getElementById('key2');
-			key2.innerText = word[2];
-			input.value = '';
-		} else if (input.value.toUpperCase() === word[3]) {
-			answer.push(input.value.toUpperCase());
-			guessCorrect()
-			smile.setAttribute('id', 'smile');
-			let key3 = document.getElementById('key3');
-			key3.innerText = word[3];
-			input.value = '';
-		} else if (input.value.toUpperCase() === word[4]) {
-			answer.push(input.value.toUpperCase());
-			guessCorrect()
-			smile.setAttribute('id', 'smile');
-			let key4 = document.getElementById('key4');
-			key4.innerText = word[4];
-			input.value = '';
-		} else {
-			incorrectGuess()
-		}
+	if (input.value.toUpperCase() === word[0]) {
+		answer.push(input.value.toUpperCase());
+		guessCorrect();
+		smile.setAttribute('id', 'smile');
+		let key0 = document.getElementById('key0');
+		key0.innerText = word[0];
+		input.value = '';
+	} else if (input.value.toUpperCase() === word[1]) {
+		answer.push(input.value.toUpperCase());
+		guessCorrect();
+		smile.setAttribute('id', 'smile');
+		let key1 = document.getElementById('key1');
+		key1.innerText = word[1];
+		input.value = '';
+	} else if (input.value.toUpperCase() === word[2]) {
+		answer.push(input.value.toUpperCase());
+		guessCorrect();
+		smile.setAttribute('id', 'smile');
+		let key2 = document.getElementById('key2');
+		key2.innerText = word[2];
+		input.value = '';
+	} else if (input.value.toUpperCase() === word[3]) {
+		answer.push(input.value.toUpperCase());
+		guessCorrect();
+		smile.setAttribute('id', 'smile');
+		let key3 = document.getElementById('key3');
+		key3.innerText = word[3];
+		input.value = '';
+	} else if (input.value.toUpperCase() === word[4]) {
+		answer.push(input.value.toUpperCase());
+		guessCorrect();
+		smile.setAttribute('id', 'smile');
+		let key4 = document.getElementById('key4');
+		key4.innerText = word[4];
+		input.value = '';
+	} else {
+		incorrectGuess();
+	}
+	console.log(answer);
+	console.log(wrongAnswers);
 }
 
 // Word 2 Submit Guess Function
@@ -206,29 +220,41 @@ function makeUnderScore() {
 
 // Check for double guesses function
 
-function checkCorrectGuess() {
+function checkDoubleCorrect() {
 	for (let i = 0; i < answer.length; i++) {
-		if(input.value.toUpperCase() === answer[i]) {
-			alert('You have already guessed this letter!')
-			input.value = ''
+		if (input.value.toUpperCase() === answer[i]) {
+			guessReaction.innerText = 'You have already guessed this letter!';
+			input.value = '';
 		}
 	}
 }
 
 // Check for double INCORRECT guesses function
 
-function checkIncorrectGuess() {
+function checkDoubleIncorrect() {
 	for (let i = 0; i < wrongAnswers.length; i++) {
 		if (input.value.toUpperCase() === wrongAnswers[i]) {
-			alert('You have already guessed this letter!');
+			guessReaction.innerText = 'You have already guessed this letter!';
 			input.value = '';
 		}
 	}
 }
 
+// Guess Reaction: Correct
+
+function guessCorrect() {
+	guessReaction.innerText = `Yay, ${input.value.toUpperCase()} was correct!`;
+}
+
+// Guess Reaction: Incorrect
+
+function guessIncorrect() {
+	guessReaction.innerText = `Oh no! ${input.value.toUpperCase()} was incorrect!`;
+}
+
 // Show Rules
 
-function showRules () {
+function showRules() {
 	alert(
 		`Welcome to Flower Time!\n \n In order to keep the flower tall and happy, you must guess its favorite word! \n\n Here are the rules: \n - In the 'Correct' box, input your guess for which letters might be found in the magic word. \n - If your guess is correct, the flower will remain intact. \n - If you guess wrong, the flower will lose a petal. \n - If the flower loses all its petals, the game is over. \n - If all of your guesses are correct and you spell the magic word, you win! \n - When you are ready, press start game. Good Luck!\n`
 	);
@@ -258,33 +284,38 @@ function removePetal() {
 	} else if (wrongAnswers.length === 7) {
 		petal7.style.backgroundColor = 'lightgreen';
 		smile.setAttribute('id', 'surprise');
-		petal7.style.zIndex = '0'
+		petal7.style.zIndex = '0';
 	} else if (wrongAnswers.length === 8) {
 		petal8.style.backgroundColor = 'lightgreen';
-		smile.setAttribute('id', 'frown')
-	}console.log(wrongAnswers)
+		smile.setAttribute('id', 'frown');
+	}
+	console.log(wrongAnswers);
 }
 
-
-// Show Cowboy Gear Function
+// Show Western Theme
 
 function showCowboy() {
-	accessoryButton.setAttribute('class', 'closed')
-	brim.style.visibility = 'visible'
-	baseLeft.style.visibility = 'visible'
-	baseRight.style.visibility = 'visible'
-	moustacheLeft.style.visibility = 'visible'
-	moustacheRight.style.visibility = 'visible'
+	brim.style.visibility = 'visible';
+	baseLeft.style.visibility = 'visible';
+	baseRight.style.visibility = 'visible';
+	moustacheLeft.style.visibility = 'visible';
+	moustacheRight.style.visibility = 'visible';
+	html.style.backgroundImage = 'url(western.jpg)';
+	html.style.backgroundColor = '#f3c086';
 }
 
-// Guess Reaction: Correct
+// Show Space Theme
 
-function guessCorrect() {
-	guessReaction.innerText = `Yay, ${input.value.toUpperCase()} was correct!`
+function showSpaceSuit() {
+	helmetTop.style.visibility = 'visible';
+	helmetBase.style.visibility = 'visible';
+	stem.style.backgroundColor = 'lightgrey';
+	leftArm.style.backgroundColor = 'lightgrey';
+	rightArm.style.backgroundColor = 'lightgrey';
+	html.style.backgroundImage = 'url(spacesuitset2.jpg)';
+	html.style.backgroundColor = '';
 }
 
-// Guess Reaction: Incorrect
+// Clear costumes function
 
-function guessIncorrect() {
-	guessReaction.innerText = `Oh no! ${input.value.toUpperCase()} was incorrect!`;
-}
+function clearCostumes() {}
